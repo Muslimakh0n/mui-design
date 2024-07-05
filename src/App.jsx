febd4717e8904d4bbb943e0ccaf20a05eb9377c2
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2024-07-05 15:59:07 +0500

    smth

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,40 @@
+// import React from 'react'
+// import {Button} from "@mui/material";
+// import {Settings} from "@mui/icons-material"
+// const App = () => {
+//   return (
+//     <div>
+
+// <Button
+// startIcon={Settings}
+// variant='contained'
+// color='secondary'
+// size='small'
+// >
+//   SEttings
+// </Button>
+//     </div>
+//   )
+// }
+
+// export default App
+
 import React from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import Switch from '@mui/material/Switch';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
+
 
 const App = () => {
 const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
-
-  const handleMenu = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
+  
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={
-            <Switch
-              checked={auth}
-              onChange={handleChange}
-              aria-label="login switch"
-            />
-          }
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
+      
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -51,46 +44,23 @@ const [auth, setAuth] = React.useState(true);
             aria-label="menu"
             sx={{ mr: 2 }}
           >
-            <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Photos
-          </Typography>
+            <div>
+              <Typography variant="h6" gutterBottom component="div" sx={{ flexGrow:1}}>
+        Sitemark
+      </Typography>
+            </div>
           {auth && (
             <div>
-              <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleMenu}
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton>
-              <Menu
-                id="menu-appbar"
-                anchorEl={anchorEl}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorEl)}
-                onClose={handleClose}
-              >
-                <MenuItem onClick={handleClose}>Profile</MenuItem>
-                <MenuItem onClick={handleClose}>My account</MenuItem>
-              </Menu>
+             
             </div>
           )}
         </Toolbar>
+       
       </AppBar>
     </Box>
+
+    
   );
 
 
